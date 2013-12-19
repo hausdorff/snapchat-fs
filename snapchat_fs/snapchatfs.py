630b05bafa68f93cd5ca1543eff1410ba3d48fc9
Alex Clemmer <clemmer.alexander@gmail.com>
2013-12-18 22:31:05 -0800

    Add documentation to `snapchatfs`

--- snapchat_fs/snapchatfs.py
+++ snapchat_fs/snapchatfs.py
@@ -6,6 +6,7 @@ downloading arbitrary data files from Snapchat.
 """
 
 import hashlib, os
+import util
 from snapchat_core import *
 
 __author__ = "Alex Clemmer, Chad Brubaker"
@@ -19,15 +20,6 @@ __email__ = "clemmer.alexander@gmail.com"
 __status__ = "Prototype"
 
 
-def bold(text):
-    return '\033[1m%s\033[0m' % text
-
-def green(text):
-    return '\033[1;32m%s\033[0m' % text
-
-def red(text):
-    return '\033[1;31m%s\033[0m' % text
-
 def sent_id_to_received_id(id):
     """
     Sent IDs have an 's' at the end, while received IDs have an 'r' on the
@@ -36,20 +28,29 @@ def sent_id_to_received_id(id):
     return id[:-1] + 'r'
 
 def download_all_sfs(username, password, target_dir):
+    """
+    Downloads all files managed by Snapchat FS, writing them to `target_dir`
+    
+    @username Username that owns the files.
+    @password Password for the account specified by `username`
+    @target_dir Where to download the files to.
+    """
     # get all downloadable files tracked by Snapchat FS
     files = all_downloadable_sfs_files(username, password)
 
+    # download each file in sequence; if we find two files with the same
+    # name, we give the file a name that includes a hash of the contents
     filenames_downloaded = set()
     for filename, content_hash, received_id, snap in files:
         try:
             data = snap.download()
             if filename not in filenames_downloaded:
                 
-                print(green("Downloading snap ") + "%s" % filename)
+                print(util.green("Downloading snap ") + "%s" % filename)
                 path = os.path.join(target_dir, filename)
             else:
-                print(green("Downloading snap ") + ("%s " % filename) +
-                      (red("but filename is not unique; ") +
+                print(util.green("Downloading snap ") + ("%s " % filename) +
+                      (util.red("but filename is not unique; ") +
                        ("downloading as: %s" %
                         (filename + "-" + content_hash))))
                 path = os.path.join(target_dir
@@ -65,6 +66,15 @@ def download_all_sfs(username, password, target_dir):
             raise
 
 def all_downloadable_sfs_files(username, password):
+    """
+    Gets all files managed in Snapchat FS for a specific user; returns
+    them as a list of Snap objects, whose IDs can be used to download
+    all or some of the files from Snapchat's DB.
+    
+    @username User who owns the files.
+    @password Password for the user.
+    @return List of Snap objects representing the files.
+    """
     ss = SfsSession(username, password)
     ss.login()
 
@@ -101,15 +111,30 @@ def all_downloadable_sfs_files(username, password):
     return sfs_files
 
 def list_all_downloadable_sfs_files(username, password):
+    """
+    Produces a list of Snap objects representing all downloadable files
+    managed by Snapchat FS for a particular user.
+    
+    @username User who owns the files.
+    @password Password for user.
+    @return List of Snap objects representing all downloadable files in SFS.
+    """
     files = all_downloadable_sfs_files(username, password)
 
-    print '\t'.join([bold('Filename'), bold('Content hash')])
+    print '\t'.join([util.bold('Filename'), util.bold('Content hash')])
     for filename, content_hash, received_id, snap in files:
         print '%s\t%s...%s' % (filename, content_hash[:17]
                                , content_hash[-3:])
 
 def upload_sfs_file(username, password, filename):
-    print green('Uploading file ') + (filename)
+    """
+    Uploads a file to Snapchat FS.
+
+    @username User who will own the file in Snapchat FS.
+    @password Password of user.
+    @filename Path of the file to upload.
+    """
+    print util.green('Uploading file ') + (filename)
     ss = SfsSession(username, password)
     ss.login()
     sfs_id = ss.generate_sfs_id(filename)
